0ac12bce43ed90728d1e9ec7599d20b1aa593e28
DHRUVBAPAT13 <bapatdhruv@gmail.com>
2026-02-05 19:43:47 +0530

    code corrections 1

--- loop.c
+++ loop.c
@@ -3,8 +3,16 @@
 
 void main()
 {
+    int num, i;
+
+    printf("Enter a number : ");
+    scanf("%d",&num);
+
+    printf("\nThe multiplication table is \n");
+
+    //below for prints the table of given number
     for(int i=1;i<=10;i++)
     {
-        printf("%d \n",i);
+        printf("%d \n",num*i);
     }
 }
